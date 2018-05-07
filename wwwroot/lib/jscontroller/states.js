851b035a079ce710fbfcf526a99db140a094e6c0
Your Name <truongquoctrong231194@gmail.com>
2018-05-07 23:51:27 +0700

    update file

--- wwwroot/lib/jscontroller/states.js
+++ wwwroot/lib/jscontroller/states.js
@@ -1,6 +1,6 @@
 ﻿var app = angular.module("App_Hotel", ['ui.router', 'oc.lazyLoad', 'ui.bootstrap', 'toaster', 'angularjs-datetime-picker']);
 app.controller("Ctrl_Hotel", function ($scope, $state, $rootScope, $http, $timeout) {
-    // demo
+    // demo    123
 });
 app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
     // Redirect any unmatched url
